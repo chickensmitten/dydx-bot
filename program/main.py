5ded97aa6c85086a68ae71af679bdc647fd3f12c
sheng yeong <chia.shengyeong@gmail.com>
2023-02-17 14:08:51 +0800

    storing cointegrated pairs

--- program/main.py
+++ program/main.py
@@ -2,6 +2,7 @@ from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 from func_connections import connect_dydx
 from func_private import abort_all_positions
 from func_public import construct_market_prices
+from func_cointegration import store_cointegration_results
 
 if __name__ == "__main__":
   # connect to client
@@ -22,8 +23,18 @@ if ABORT_ALL_POSITIONS:
 
 if FIND_COINTEGRATED:
   try:
-    print("fetching market prices, please allow 3 mins... ")
+    print("Fetching market prices, please allow 3 mins... ")
     df_market_prices = construct_market_prices(client)
+  except Exception as e:
+    print("Error constructing market prices: ", e)
+    exit(1)
+
+  try:
+    print("Storing cointegrated pairs... ")
+    stores_result = store_cointegration_results(df_market_prices)
+    if stores_result != "saved":
+      print("Error saving cointegrated pairs")
+      exit(1)
   except Exception as e:
     print("Error constructing market prices: ", e)
     exit(1)
