44075d6c2f3afdd7c96c3adbe92376160cfbee27
sheng yeong <chia.shengyeong@gmail.com>
2023-02-17 13:51:45 +0800

    added public functions to get data

--- program/func_private.py
+++ program/func_private.py
@@ -42,7 +42,7 @@ def place_market_order(client, market, side, size, price, reduce_only):
 
   # Get expiration time
   server_time = client.public.get_time()
-  expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(seconds=28870) # there is some GMT issue, have to add 8 hours or 28800
+  expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(seconds=70) # there is some GMT issue, have to add 8 hours or 28800
 
   # Place an order
   placed_order = client.private.create_order(
--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
-from constants import ABORT_ALL_POSITIONS
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 from func_connections import connect_dydx
 from func_private import abort_all_positions
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
   # connect to client
@@ -18,3 +19,11 @@ if ABORT_ALL_POSITIONS:
   except Exception as e:
     print("Error closing all positions: ", e)
     exit(1)    
+
+if FIND_COINTEGRATED:
+  try:
+    print("fetching market prices, please allow 3 mins... ")
+    df_market_prices = construct_market_prices(client)
+  except Exception as e:
+    print("Error constructing market prices: ", e)
+    exit(1)
